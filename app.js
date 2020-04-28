c5fada2ab244e257d8af04bde2189d643f366468
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-04-29 01:22:47 +0200

    parse json

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const request =require('request')
-const url = 'https://api.darksky.net/forecast/5730d209a25118fa5bc7c3dc410a32c2/37.8267,-122.4233'
-request({url:url},(error,response)=>{
-     const data = JSON.parse(response.body)
-     console.log(data.currently);
+const url = 'https://api.darksky.net/forecast/5730d209a25118fa5bc7c3dc410a32c2/37.8267,-122.4233?lang=en'
+request({url:url,json:true},(error,response)=>{
+     const data = response.body
+     console.log(`${data.daily.data[0].summary} It is currently ${data.currently.temperature} degrees out . There is a ${data.currently.precipProbability} chance of rain .`);
+     
     
     
 })
