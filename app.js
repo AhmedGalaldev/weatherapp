66d87bd96e8dd3e055f6dbb67265896cea2543e1
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-05-01 01:18:38 +0200

    callback channing

--- app.js
+++ app.js
@@ -2,16 +2,36 @@
 const geoCode = require('./utils/geocode')
 const foreCast = require('./utils/forecast')
 
-geoCode("CAIRO",(error,data)=>{
-    console.log("Error ",error);
-    console.log("Data ",data);
-    
-    
-})
 
-foreCast(-75.7088,44.1545,(error,data)=>{
-    console.log("Error ",error);
-    console.log("Data ",data);
+
+const address = process.argv[2]
+if(!address){
+    console.log("there is no address");
+    
+}else{
+    geoCode(address,(error,data)=>{
+   
+        if(error){
+          return  console.log(error);
+            
+        } else{
+            foreCast(data.longitude,data.lititude,(error,foreCasteData)=>{
+                if(error){
+                    return console.log(error);
+                    
+                }else{
+
+                    console.log("Data ",foreCasteData);
+                    console.log(data.location);
+                    
+                }
+                
+                
+            })
+        }   
     
     
 })
+}
+
+
